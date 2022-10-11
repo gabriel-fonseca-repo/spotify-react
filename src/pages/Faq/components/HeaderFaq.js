6eeaf73441ffebaca05d197957e83e247ab63c5c
gabriel.fonseca <gabriel.fonseca@hmit.com.br>
2022-10-11 12:59:36 -0300

    configurações do prettier

--- src/pages/Faq/components/HeaderFaq.js
+++ src/pages/Faq/components/HeaderFaq.js
@@ -2,23 +2,23 @@ import styles from "../Faq.module.css";
 import { Link } from "react-router-dom";
 
 export function HeaderFaq() {
-  return (
-    <header>
-      <div className={styles.logo_container}>
-        <Link to="/">
-          <img
-            className={styles.logo}
-            src="/assets/svg/logo.svg"
-            alt="Logo do spotify"
-          />
-        </Link>
-      </div>
-      <div className={styles.subscribe_login_container}>
-        <Link to="/cadastro">
-          <span className={styles.subscribe_button}>Inscreva-se</span>
-        </Link>
-        <span className={styles.login_button}>Entrar</span>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className={styles.logo_container}>
+				<Link to="/">
+					<img
+						className={styles.logo}
+						src="/assets/svg/logo.svg"
+						alt="Logo do spotify"
+					/>
+				</Link>
+			</div>
+			<div className={styles.subscribe_login_container}>
+				<Link to="/cadastro">
+					<span className={styles.subscribe_button}>Inscreva-se</span>
+				</Link>
+				<span className={styles.login_button}>Entrar</span>
+			</div>
+		</header>
+	);
 }
--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -3,12 +3,12 @@ import { ContentFaq } from "./components/ContentFaq";
 import styles from "./Faq.module.css";
 
 export function Faq() {
-  return (
-    <div className={styles.container}>
-      <main>
-        <HeaderFaq></HeaderFaq>
-        <ContentFaq></ContentFaq>
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<main>
+				<HeaderFaq></HeaderFaq>
+				<ContentFaq></ContentFaq>
+			</main>
+		</div>
+	);
 }
