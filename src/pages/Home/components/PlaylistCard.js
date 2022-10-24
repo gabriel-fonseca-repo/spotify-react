8e0377d7807fe1f86ac8374f6b4699bc7317aa25
gabriel.fonseca <gabrielfonseca@edu.unifor.br>
2022-10-24 20:59:01 -0300

    Ajustes finais para entrega

--- src/pages/Home/components/PlaylistCard.js
+++ src/pages/Home/components/PlaylistCard.js
@@ -2,29 +2,25 @@ import { Link } from "react-router-dom";
 import styles from "../Home.module.css";
 
 export function PlaylistCard(props) {
-  return (
-    <div>
-      <Link
-        className={styles.playlist_card_link}
-        to={"/playlist/" + props.id}
-      >
-        <div className={styles.music_list_card}>
-          <img src={props.src} alt={props.alt} />
-          <div className={styles.card_details}>
-            <div className={styles.title}>{props.title}</div>
-            <div className={styles.detail}>{props.detail}</div>
-          </div>
-        </div>
-      </Link>
-      
-      {props.editable ? (
-        <Link to={"/criarplaylist/" + props.id}>
-          editar
-        </Link>
-      ) : (
-        <></>
-      )}
-
-    </div>
-  );
+	return (
+		<div>
+			<div className={styles.music_list_card}>
+				<Link
+					className={styles.playlist_card_link}
+					to={"/playlist/" + props.id}
+				>
+					<img src={props.src} alt={props.alt} />
+					<div className={styles.card_details}>
+						<div className={styles.title}>{props.title}</div>
+						<div className={styles.detail}>{props.detail}</div>
+					</div>
+				</Link>
+				{props.editable ? (
+					<Link to={"/criarplaylist/" + props.id}>editar</Link>
+				) : (
+					<></>
+				)}
+			</div>
+		</div>
+	);
 }
